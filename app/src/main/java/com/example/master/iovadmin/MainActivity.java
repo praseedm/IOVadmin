e878214a1da0f2484d9b998c7e538e161fadab02
praseedm <prasi.praseed@gmail.com>
2017-05-24 16:11:22 +0530

    With map

--- app/src/main/java/com/example/master/iovadmin/MainActivity.java
+++ app/src/main/java/com/example/master/iovadmin/MainActivity.java
@@ -1,37 +1,72 @@
 package com.example.master.iovadmin;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     DatabaseReference database = FirebaseDatabase.getInstance().getReference();
     DatabaseReference announRef = database.child("announcement");
     TextView textview;
+    private GoogleMap mMap;
+    static String TAG = "mainactivity";
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview = (TextView) findViewById(R.id.textview);
-        announRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String msg = dataSnapshot.getValue().toString();
-                textview.setText(msg);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setUpMap();
+    }
+
+    private void setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+    }
+
+    private void setUpMapIfNeeded() {
+
+        try {
+            if (mMap == null) {
+
+                Log.d(TAG, "setUpMapIfNeeded");
+
+                mapFragment = (MapFragment) getFragmentManager()
+                        .findFragmentById(R.id.mapview);
+                mapFragment.getMapAsync(this);
             }
+        } catch (Exception e) {
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+            e.printStackTrace();
+        }
     }
 }
